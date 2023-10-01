709e1df758ece0a815202d832cbd6756e1b04e63
sakshikaleves <96903857+sakshikaleves@users.noreply.github.com>
2023-10-01 23:30:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+//Create a react app from scratch.
+
+import ReactDOM from "react-dom";
+import React from "react";
+
+let name = "sakshi";
+const cr = new Date().getFullYear();
+const new2 = new Date().toString(); // Convert the Date object to a string
+
+console.log(new2);
+
+ReactDOM.render(
+  <div>
+    <h1>welcome</h1>
+    <footer>
+      <ol>
+        <p>website created by {name}</p>
+        <p>copyright {cr}</p>
+        {/* <p>current time: {new2}</p> Render the string representation of the Date */}
+      </ol>
+    </footer>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
