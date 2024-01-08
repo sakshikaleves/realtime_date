dff17302e181a9a0cbc1506dea3e0ddffbe6d048
sakshikaleves <96903857+sakshikaleves@users.noreply.github.com>
2024-01-08 17:03:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,11 +22,3 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
-
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
